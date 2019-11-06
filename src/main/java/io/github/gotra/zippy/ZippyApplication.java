b9a700280bc0ec6e5f354bdc33e22d7d0b138e5b
gururra <rajeev.guru@ext.ec.europa.eu>
2019-11-06 17:00:35 +0100

    Initial commit

---
+++ src/main/java/io/github/gotra/zippy/ZippyApplication.java
@@ -0,0 +1,15 @@
+package io.github.gotra.zippy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZippyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZippyApplication.class, args);
+    }
+
+
+
+}
